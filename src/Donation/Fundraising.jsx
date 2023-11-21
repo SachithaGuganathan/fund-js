70f71d89927d57b8df5e59ea9c4ded1c9427a3ca
SachithaJungkook <727722euit155@skcet.ac.in>
2023-11-21 18:17:23 +0530

    commit

---
+++ src/Donation/Fundraising.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Homepage from './Homepage'
+import { Link } from "react-router-dom";
+const Fundraising = () => {
+  return (
+    <div className='fr'>
+        <center>
+        <h1 style={{fontStyle:'italic',fontSize:40}}>FUND ANYTHING</h1>
+        <br/><br/>
+        <i className='lmtwo'>School Fundraising</i><br/>
+        <i className='lmthree'>Donate amount for tuition,school studies,etc..</i><br/><br/>
+        <i className='lmtwo'>School Fundraising</i><br/>
+        <i className='lmthree'>Donate amount for tuition,school studies,etc..</i><br/><br/>
+        <i className='lmtwo'>Sports Fundraising</i><br/>
+        <i className='lmthree'>Donate amount for teams and atheletes ,etc..</i><br/><br/>
+        <i className='lmtwo'>Nonprofits and Charities</i><br/>
+        <i className='lmthree'>Donate amount for community engagement</i><br/><br/>
+        <i className='lmtwo'>Corporate Social</i><br/>
+        <i className='lmthree'>Donate for the self-goals of the volunteers..</i><br/><br/>
+        <i className='lmtwo'>Animal Fundraising</i><br/>
+        <i className='lmthree'>Donate amount for welfare of the animals</i><br/><br/>
+        <i className='lmtwo'>Social Enterprises</i><br/>
+        <i className='lmthree'>Donate for social or environmental objectives</i><br/><br/>
+        <i className='lmtwo'>Crowdfunding as a Service</i><br/>
+        <i className='lmthree'>Donating money to projects and businesses.</i><br/><br/>
+        </center>
+    </div>
+  )
+}
+
+export default Fundraising
